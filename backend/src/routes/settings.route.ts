8c011f4eaba891c366bd81dd563e0838df0e86b4
Akdev Saha <akdevsaha2003@gmail.com>
2025-11-09 19:28:29 +0530

    chore: add the multer middleware to setProfilePicture route

--- backend/src/routes/settings.route.ts
+++ backend/src/routes/settings.route.ts
@@ -1,9 +1,9 @@
 import express from "express"
-import { setAbout, setProfilePicture } from "../controllers/setttings.controller.js"
+import { multerMiddleware, setAbout, setProfilePicture } from "../controllers/setttings.controller.js"
 
 const router = express.Router()
 
-router.post("/setProfilePicture",  ,  setProfilePicture)
+router.post("/setProfilePicture", multerMiddleware, setProfilePicture)
 router.post("/setAbout", setAbout)
 
 export default router
