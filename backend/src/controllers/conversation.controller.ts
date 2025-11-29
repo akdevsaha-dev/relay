a7618492e3751cd530b2085c365e080cbd3ec532
Akdev Saha <akdevsaha2003@gmail.com>
2025-11-29 16:32:39 +0530

    feat: add route to get a conversation detail

--- backend/src/controllers/conversation.controller.ts
+++ backend/src/controllers/conversation.controller.ts
@@ -1,9 +1,10 @@
 import type { Request, Response } from "express";
 import prisma from "../db/client.js";
+import { success } from "zod";
 
 export const createConversation = async (req: Request, res: Response) => {
-    const { senderId, recieverId } = req.body
-    console.log(senderId, recieverId)
+    const senderId = req.user?.id
+    const { recieverId } = req.body
     try {
         if (!senderId || !recieverId) {
             return res.status(400).json({
@@ -129,7 +130,6 @@ export const createGroupConversation = async (req: Request, res: Response) => {
             });
         }
 
-        // Avoid creator duplication
         const uniqueMembers = groupMemberIds.filter((id: string) => id !== creatorId);
 
         const group = await prisma.conversation.create({
@@ -168,3 +168,45 @@ export const createGroupConversation = async (req: Request, res: Response) => {
         });
     }
 };
+
+
+export const getConversation = async (req: Request, res: Response) => {
+    const { conversationId } = req.params;
+    try {
+        if (!conversationId) {
+            return res.status(400).json({
+                error: "No conversation Id"
+            })
+        }
+        const conversation = await prisma.conversation.findUnique({
+            where: {
+                id: conversationId
+            },
+            include: {
+                participants: {
+                    include: {
+                        user: {
+                            select: {
+                                id: true,
+                                userName: true,
+                                avatarUrl: true,
+                                email: true,
+                                lastSeen: true,
+                                status: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        return res.status(200).json({
+            success: true,
+            conversation: conversation
+        })
+    } catch (error) {
+        return res.status(400).json({
+            error: "Server error"
+        })
+    }
+
+}
--- backend/src/routes/conversation.route.ts
+++ backend/src/routes/conversation.route.ts
@@ -1,11 +1,12 @@
 import express from "express"
-import { createConversation, createGroupConversation, getAllConversations } from "../controllers/conversation.controller.js"
+import { createConversation, createGroupConversation, getAllConversations, getConversation } from "../controllers/conversation.controller.js"
 import { protectRoute } from "../middleware/protectRoute.js"
 
 const router = express.Router()
 
-router.post("/create", createConversation)
+router.post("/create", protectRoute, createConversation)
 router.get("/getConversations", protectRoute, getAllConversations)
 router.post("/createGroup", createGroupConversation)
+router.get("/:conversationId", getConversation)
 
 export default router
